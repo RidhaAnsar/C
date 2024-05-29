293b5885edcc1eecb76294b894e3068e4f2868b3
Ridha Ansar <140969987+RidhaAnsar@users.noreply.github.com>
2024-05-29 23:35:49 +0530

    Consonants to Uppercase.c

---
+++ Consonants to Uppercase.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+#define MAX 100
+
+char * function(char * str)
+{
+    
+    {
+    for(int i=0;i<strlen(str);i++)
+    {
+        if( str[i]!='a' && str[i]!='e' && str[i]!='i' && str[i]!='o' && str[i]!='u' && str[i]!='A' && str[i]!='E' && str[i]!='I' && str[i]!='O' && str[i]!='U')
+        {
+            str[i] = toupper(str[i]);
+        }
+    }
+    return str;
+}
+}
+
+
+void main()
+{   
+    char text[MAX];
+    fgets(text, sizeof(text), stdin);
+    
+    char * out=function(text);
+    printf("%s",out);
+}
